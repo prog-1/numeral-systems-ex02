08ed8354ed54beba022cb54f02596d2eb09567ac
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2022-02-13 22:49:55 +0200

    the second task (from base 10) is done

---
+++ from_base10/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func fromBase10(num int, toBase int) string {
+	var baseB string
+	if 2 <= toBase && toBase <= 36 {
+		k, power := 1, -1 // because "for" makes 1 extra move
+		for ; k <= num; k = k * toBase {
+			power++
+		}
+
+		l := int(math.Pow(float64(toBase), float64(power)))
+		var times int
+		base36 := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		for power >= 0 {
+			if l <= num {
+				l = l + int(math.Pow(float64(toBase), float64(power)))
+				times++
+			} else {
+				if toBase >= 10 {
+					baseB = baseB + string(base36[times])
+				} else {
+					baseB = baseB + fmt.Sprint(times)
+				}
+				times = 0
+				num = num - (l - int(math.Pow(float64(toBase), float64(power)))) // because we have an extra "int(math.Pow(float64(toBase), float64(power)))" here
+				power--
+				l = int(math.Pow(float64(toBase), float64(power)))
+
+			}
+		}
+	} else {
+		baseB = "-1"
+	}
+	return baseB
+}
+
+func main() {
+	var num int
+	fmt.Print("Enter number: ")
+	fmt.Scan(&num)
+	var toBase int
+	fmt.Print("Enter base: ")
+	fmt.Scan(&toBase)
+	if fromBase10(num, toBase) != "-1" {
+		fmt.Printf("The number in base %d: %v\n", toBase, fromBase10(num, toBase))
+	} else {
+		fmt.Println("ERROR: Incorrect base")
+	}
+}
